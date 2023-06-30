5b7abbab190adda3997461f001c22bf72d46b678
dave <dcurtin91@gmail.com>
2023-06-30 16:45:34 -0500

    new

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/david-curtin/",
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path'; // Add this line
+
 export default defineConfig({
   plugins: [react()],
-  base: "/david-curtin/",
-})
-
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'public/404.html'),
+      },
+    },
+  },
+});
 
