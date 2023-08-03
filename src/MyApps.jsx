942b5c26c26b7cb3deb67180a29288d4d12bcb45
dave <dcurtin91@gmail.com>
2023-08-02 19:15:46 -0500

    python

--- src/MyApps.jsx
+++ src/MyApps.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-scroll";
 import image from "./speccheck.png";
 import image2 from "./GPTloop.png";
 import image3 from "./memberportal.png";
+import image4 from "./spectralcentroid.png";
 // import Summary from "./Summary";
 // import SummaryTwo from "./SummaryTwo";
 import "./App.css";
@@ -132,34 +133,34 @@ function MyApps() {
             </a>
           </div>
           {/* <div class="tile is-child box">
-                    <a href="/apps/spectralcentroid">Spectral Centroid</a>
+                    <a href="http://dcurtin91.pythonanywhere.com/">Spectral Centroid</a>
                 </div> */}
         </div>
 
-        {/* <div className="tile is-6 is-vertical is-parent">
+        <div className="tile is-6 is-vertical is-parent">
           <div className="tile is-child box">
             <h1>
               <a
-                href="https://dcurtin91.github.io/spec-check/"
+                href="http://dcurtin91.pythonanywhere.com/"
                 target="_blank"
                 rel="noreferrer"
                 className="apps"
               >
-                Spec Check
+                Spectral Centroid
               </a>
             </h1>
-            <div className="summary2">
-              <SummaryTwo />
+            <div className="summary">
+              *Work in progress
             </div>
             <a
-              href="https://dcurtin91.github.io/spec-check/"
+              href="http://dcurtin91.pythonanywhere.com/"
               target="_blank"
               rel="noreferrer"
             >
-              <img src={image} alt="speccheck" />
+              <img src={image4} alt="speccheck" />
             </a>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
