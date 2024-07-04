e6f3493fffa2477a287a42181623d43e62f0cb67
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-07-04 15:15:31 -0500

    main

--- src/Music_New.jsx
+++ src/Music_New.jsx
@@ -1,7 +1,10 @@
 function MusicNew() {
     return(
         <div className="container">
-            <div className="bandcamps"></div>
+            <div className="bandcamps">
+                test 
+            <iframe src="https://bandcamp.com/EmbeddedPlayer/track=3904961062/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://davidcurtin.bandcamp.com/album/cms">CMS by David Curtin</a></iframe>
+            </div>
             <div className="bandcamps"></div>
         </div>
     )
