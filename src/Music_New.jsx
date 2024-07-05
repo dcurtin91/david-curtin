e77cd3823e25878b8a43e5ac30f8e43ee1aab127
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-07-05 11:16:46 -0500

    fix format

--- src/Music_New.jsx
+++ src/Music_New.jsx
@@ -1,6 +1,6 @@
 function MusicNew() {
     return(
-        <div className="container">
+        <div className="containa">
             <div className="bandcamps">
                 test 
             <iframe src="https://bandcamp.com/EmbeddedPlayer/track=3904961062/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://davidcurtin.bandcamp.com/album/cms">CMS by David Curtin</a></iframe>
